fd7667acbec7e1fac3af06315274a902c665c09f
numieow <maximus.wirth@outlook.fr>
2024-03-12 16:08:21 -0400

    [+] Ajout longueur des extraits dans csv

--- src/build_csv.py
+++ src/build_csv.py
@@ -6,6 +6,8 @@ import csv
 import re
 import pandas as pd
 import numpy as np
+import librosa
+import tqdm
 
 TEST_PATHS = ['Test/Part1/', 'Test/Part2/', 'Test/Part3/']
 TRAIN_SUBFILES = ['cel', 'cla', 'flu', 'gac', 'gel', 'org', 'pia', 'sax', 'tru', 'vio', 'voi']
@@ -21,20 +23,24 @@ def build_test_csv(folder_paths, csv_file_name):
         writer = csv.writer(csvfile)
 
         # Write the header
-        writer.writerow(['Track Name', 'Instrument'])
+        writer.writerow(['Track Name', 'Instrument, Duration'])
 
         # Go through the files and extract the data
         for i in range(len(folder_paths)):
-            for file in files[i]:   
+            for file in tqdm.tqdm(files[i]):   
                 if file.endswith('.txt'):
                     with open(folder_paths[i] + file, 'r') as f:
                         lines = f.readlines()
+                        y, sr = librosa.load(folder_paths[i] + file.split('.txt')[0] + '.wav')
+                        duration = librosa.get_duration(y=y, sr=sr)
                         for line in lines:
                             file_name = folder_paths[i] + file.split('.txt')[0]
-                            writer.writerow([file_name, line.strip()])
+                            writer.writerow([file_name, line.strip(), duration])
+            print("FINI LISTE")
 
     # Read the csv file and return the dataframe
     df = pd.read_csv(csv_file_name)
+    print("FINI TEST BUILD")
     return df
 
 def build_train_csv(folder_paths, csv_file_name):
@@ -46,11 +52,13 @@ def build_train_csv(folder_paths, csv_file_name):
         writer = csv.writer(csvfile)
 
         # Write the header
-        writer.writerow(['Track Name', 'Instrument'])
+        writer.writerow(['Track Name', 'Instrument, Duration'])
 
         # Go through the files and extract the data
         for i in range(len(folder_paths)):
-            for file in files[i]:
+            for file in tqdm.tqdm(files[i]):
+                y, sr = librosa.load(folder_paths[i] + file)
+                duration = librosa.get_duration(y=y, sr=sr)
                 file = file.split('.wav')[0]
                 first_bracket = file.find('[')
                 last_bracket = file.rfind(']')
@@ -70,11 +78,11 @@ def build_train_csv(folder_paths, csv_file_name):
                     instruments = instruments[close_brack+1:]
 
                 for instrument in instrument_names:
-                    writer.writerow([file_name, instrument])
+                    writer.writerow([file_name, instrument, duration])
 
     # Read the csv file and return the dataframe
     df = pd.read_csv(csv_file_name)
     return df
 
 #build_test_csv(TEST_PATHS, 'test_data.csv')
-#build_train_csv(TRAIN_PATHS, 'train_data.csv')
+build_train_csv(TRAIN_PATHS, 'train_data.csv')
--- src/build_model.py
+++ src/build_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import librosa
+import keras
+import matplotlib.pyplot as plt
 
 INSTRUMENTS_FULL_NAME = {
     'cel':'cello',
@@ -27,6 +29,10 @@ def feature_extract(file):
     
     #get wave representation : y : waveform, s : sampling rate
     y, sr = librosa.load(file)
+
+    # Print duration of file
+    duration = librosa.get_duration(y=y, sr=sr)
+    print("Duration: ", duration)
         
     #determine if instrument is harmonic or percussive by comparing means
     y_harmonic, y_percussive = librosa.effects.hpss(y)
@@ -41,9 +47,18 @@ def feature_extract(file):
     mfcc=np.mean(mfcc,axis=1)
     
     #get the mel-scaled spectrogram
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)  
+    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=45000)  
     #temporally average spectrogram
-    spectrogram = np.mean(spectrogram, axis = 1)
+    #spectrogram = np.mean(spectrogram, axis = 1)
+
+
+    #Specshow the spectrogram
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max), y_axis='mel', fmax=45000, x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel spectrogram')
+    plt.tight_layout()
+    plt.show()
     
     #compute chroma energy
     chroma = librosa.feature.chroma_cens(y=y, sr=sr)
@@ -55,3 +70,31 @@ def feature_extract(file):
     contrast = np.mean(contrast, axis= 1)
     
     return [harmonic, mfcc, spectrogram, chroma, contrast]
+
+_, _, s, _, _ = feature_extract("../data/Test/Part2/15_more than a feeling - boston-6.wav")
+print(s.shape)
+
+model = keras.models.Sequential(
+    [
+        keras.layers.Input(shape=(28, 28, 1)),
+        keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Flatten(),
+        keras.layers.Dense(500, activation='relu'),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(len(INSTRUMENTS_FULL_NAME), activation='softmax')
+    ]
+)
+
+""" EXTRAIRE DES FICHIERS WAV D'UNE CERTAINE LONGUEUR
+from pydub import AudioSegment
+t1 = t1 * 1000 #Works in milliseconds
+t2 = t2 * 1000
+newAudio = AudioSegment.from_wav("oldSong.wav")
+newAudio = newAudio[t1:t2]
+newAudio.export('newSong.wav', format="wav")"""
